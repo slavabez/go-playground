d0b34a9ac06f8e847205bda884289f953d032f65
Slava Bezgachev <bezgachev@gmail.com>
2018-06-13 12:12:45 +0600

    The small deck of cards project with simple tests

---
+++ cards/deck_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"testing"
+	"os"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+	if len(d) != 16 {
+		t.Errorf("Wrong number of cards, expected 16, got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card to be Ace of Spades, got %v", d[0])
+	}
+
+	if d[len(d) - 1] != "Four of Clubs" {
+		t.Errorf("Expected the last card to be Four of Clubs, got %v", d[len(d) - 1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T){
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in the deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+
+
+
+}
